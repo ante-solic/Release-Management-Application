a4e6d27bcd20abd55d8b0020ecfd2f84c8c8d013
ante-solic <92752482+ante-solic@users.noreply.github.com>
2024-03-23 20:02:41 +0100

    Spring boot setup

---
+++ ReleaseManagement/src/test/java/com/asolic/ReleaseManagement/ReleaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asolic.ReleaseManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
